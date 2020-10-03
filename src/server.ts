9aaa24216906a604a1127d6ae30bc7faf9bf4986
nguyen duc luong phat <nguyenducluongphat7049@gmail.com>
2020-10-04 03:13:01 +0700

    update

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Application } from 'express';
 import root from './root';
 import { env } from './config';
+import path from 'path';
 // app
 const app: Application = express();
 
@@ -13,6 +14,15 @@ root(app)
 
 // config
 const PORT = env.PORT || 5000;
+
+// 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 // start server
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
